01aa1527d52fe42739452851b0cbd1e4d38cf5b9
Jackula <jackula@gmail.com>
2020-12-06 20:31:15 +1100

    update to original source

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
 
-test('renders learn react link', () => {});
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
